72038a61bf762c24b98dfc68a769615a040e1d7f
camgloria <cam_gloria@yahoo.com>
2024-10-29 16:12:09 -0400

    Create base project files.

---
+++ WordPlay/Models/Stats.swift
@@ -0,0 +1,17 @@
+//
+//  Stats.swift
+//  WordPlay
+//
+//  Created by Camila Gloria on 10/29/24.
+//
+
+import Foundation
+
+struct Stats {
+    var username: String
+    var streak: Int
+    var totalWon: Int
+    var totalGames: Int
+    var lastGameDate: Date
+    var defaultSize: Int
+}
---
+++ WordPlay/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  WordPlay
+//
+//  Created by Camila Gloria on 10/29/24.
+//
+
+import Foundation
+
+struct User {
+    var username: String
+    var password: String
+}
