429e17c54d744c62cc5ac908e455c35c049c489b
camgloria <cam_gloria@yahoo.com>
2024-10-29 15:46:01 -0400

    Initial Commit

---
+++ WordPlay/WordPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordPlayApp.swift
+//  WordPlay
+//
+//  Created by Camila Gloria on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct WordPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
