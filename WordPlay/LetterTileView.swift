c7a94927a2d26356c4e82efb1c3485373ca7e2fc
Elysse Viena <elysseviena23@gmail.com>
2024-10-30 19:57:35 -0400

    Added code to make one tile. working on adding more default tiles

---
+++ WordPlay/LetterTileView.swift
@@ -0,0 +1,18 @@
+//
+//  LetterTileView.swift
+//  WordPlay
+//
+//  Created by Elysse V on 10/30/24.
+//
+
+import SwiftUI
+
+struct LetterTileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LetterTileView()
+}
