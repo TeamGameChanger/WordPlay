d0f145f3bdec3db29356270efccf37cda18e37e5
Emma Chu <echu005@fiu.edu>
2024-10-30 18:26:39 -0400

    commiting the rest of completed changes

--- WordPlay/WordPlayApp.swift
+++ WordPlay/WordPlayApp.swift
@@ -5,13 +5,42 @@
 //  Created by Camila Gloria on 10/29/24.
 //
 
+
 import SwiftUI
+import FirebaseCore // <-- Import Firebase
 
 @main
 struct WordPlayApp: App {
+    @State private var authManager: AuthManager
+    
+    init() { // <-- Add an init
+        FirebaseApp.configure() // <-- Configure Firebase app
+        authManager = AuthManager()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if authManager.user != nil { // <-- Check if you have a non-nil user (means there is a logged in user)
+
+                // We have a logged in user, go to ChatView
+                NavigationStack {
+                    Text("Welcome to WordPlay!")
+                        .navigationTitle("Chat")
+                        .navigationBarTitleDisplayMode(.inline)
+                        .toolbar {
+                            ToolbarItem {
+                                Button("Sign out") {
+                                    authManager.signOut()
+                                }
+                            }
+                        }
+                }
+            } else {
+                ContentView().environment(authManager)
+            }
         }
     }
 }
+
+
+
