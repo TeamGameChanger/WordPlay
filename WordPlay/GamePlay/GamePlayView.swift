88b5c1f780d419d8969276fb0f435d98b75c03b4
Elysse Viena <elysseviena23@gmail.com>
2024-11-04 14:02:58 -0500

    Added non functional backspace button. working on adding functionality

--- WordPlay/GamePlay/GamePlayView.swift
+++ WordPlay/GamePlay/GamePlayView.swift
@@ -19,12 +19,12 @@ struct GamePlayView: View {
     let targetWord = "CRATE" //TODO: replace with the actual word needed to solve the wordplay
     
     var body: some View {
+        Spacer()
+        
         GridView(tiles: $gridTiles)
         
         Spacer()
         
-        // TODO: Backspace button
-        // TODO: Change keyboard colors only when word is submitted
         // TODO: Do not override green or dark gray keyboard tile colors
         KeyboardView(enteredLetters: $currentInput,
                      gameOver: $gameOver, submitPressed: $submitPressed,
@@ -42,31 +42,50 @@ struct GamePlayView: View {
                     }//end of for loop
                 }//end of if not gameover
             }//end of on change
-        
-        Button("Submit") {
-            if !gameOver && currentInput.count == 5 {
-                submitPressed.toggle() //not really paying attention to the value as much as whether it changed or not
-                updateTileColors()
-                
-                if currentInput == targetWord {
-                    print("Game win")
-                    gameOver = true
-                    // TODO: Trigger game end code
-                }
-                
-                // TODO: Check if input is a valid word
-                
-                currentRow += 1
-                currentInput = ""
-                
-                if currentRow > 5 {
-                    print("Game end")
-                    gameOver = true
-                    // TODO: Trigger game end code
+        Spacer()
+        HStack{
+            Spacer()
+            
+            Button("Submit") {
+                if !gameOver && currentInput.count == 5 {
+                    submitPressed.toggle()//needed for keyboard to update
+                    updateTileColors()
+                    
+                    if currentInput == targetWord {
+                        print("Game win")
+                        gameOver = true
+                        // TODO: Trigger game end code
+                    }
+                    
+                    // TODO: Check if input is a valid word
+                    
+                    currentRow += 1
+                    currentInput = ""
+                    
+                    if currentRow > 5 {
+                        print("Game end")
+                        gameOver = true
+                        // TODO: Trigger game end code
+                        //😦
+                    }
                 }
             }
-        }
-        .buttonStyle(.borderedProminent)
+            .buttonStyle(.borderedProminent)
+            
+            Spacer()
+            
+            // TODO: Backspace button
+            Button{
+                print("delete pressed")
+                if !gameOver && currentInput.count > 0 {
+                    
+                }
+            } label: {Image(systemName: "delete.backward")}
+            .buttonStyle(.bordered)
+            
+            Spacer()
+        }//end of Hstack
+        Spacer()
     }
     
     // determines what color the grid tiles should be after submitting a word
